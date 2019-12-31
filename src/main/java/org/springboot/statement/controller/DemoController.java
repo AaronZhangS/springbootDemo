bde4c35c0aa9cb1b9c8b07e541f545fbb1dc21b2
zhangsheng <zhangsheng@ac361.com>
2019-12-31 15:51:04 +0800

    忽视一些警告

--- src/main/java/org/springboot/statement/controller/DemoController.java
+++ src/main/java/org/springboot/statement/controller/DemoController.java
@@ -1,8 +1,5 @@
 package org.springboot.statement.controller;
 
-import java.io.File;
-
-import org.springboot.statement.objectstore.MinioObjectClient;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -19,8 +16,6 @@ public class DemoController {
 	@RequestMapping("/minio")
 	@ResponseBody
 	public String minio(){
-		// File file = new File("/object_store_config.properties");
-		MinioObjectClient.getInstance().saveFile(null, null);
 		return "hello spring boot";
 	}
 }
