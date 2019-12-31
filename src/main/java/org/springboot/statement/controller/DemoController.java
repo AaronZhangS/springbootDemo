c1763650201983d8fcf07fc1c17b234463f819b0
zhangsheng <zhangsheng@ac361.com>
2019-12-31 11:54:02 +0800

    init

---
+++ src/main/java/org/springboot/statement/controller/DemoController.java
@@ -0,0 +1,15 @@
+package org.springboot.statement.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class DemoController {
+	
+	@RequestMapping("/index")
+    @ResponseBody
+    public String index(){
+        return "hello spring boot";
+    }
+}
