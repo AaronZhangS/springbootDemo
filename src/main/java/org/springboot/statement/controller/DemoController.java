c3834c253353d6d5d52c2b3c07d4ee38599e5ed2
zhangsheng <zhangsheng@ac361.com>
2020-01-06 12:07:17 +0800

    邮箱验证demo完成

--- src/main/java/org/springboot/statement/controller/DemoController.java
+++ src/main/java/org/springboot/statement/controller/DemoController.java
@@ -18,80 +18,94 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class DemoController {
-	
+
 	@Autowired
 	private TeamMapper teamMapper;
-	
+
 	@Autowired
 	private UserServiceImpl userServiceImpl;
-	
+
 	@RequestMapping("/")
-    @ResponseBody
-    public String hello(){
-        return "hello spring boot";
-    }
-	
+	@ResponseBody
+	public String hello() {
+		return "hello spring boot";
+	}
+
 	@RequestMapping("/minio")
 	@ResponseBody
-	public List<Team> minio(){
+	public List<Team> minio() {
 		return teamMapper.selectAll();
 	}
-	
+
 	@RequestMapping("/login")
-	public String login(){
+	public String login() {
 		return "Login.html";
 	}
-	
+
 	@RequestMapping("/regist")
-	public String regist(){
+	public String regist() {
 		return "Regist.html";
 	}
-	
+
+	@RequestMapping("/failure")
+	public String failure() {
+		return "Failure.html";
+	}
+
+	@RequestMapping("/success")
+	public String success() {
+		return "Success.html";
+	}
+
 	@RequestMapping("/index")
-	public String index(){
+	public String index() {
 		return "Index.html";
 	}
-	
+
 	@RequestMapping("/regist/verify")
 	@ResponseBody
-	public int registVerify(HttpServletRequest request){
+	public int registVerify(HttpServletRequest request) {
 		int state = 0;
 		String username = request.getParameter("username");
 		String password = request.getParameter("password");
 		String email = request.getParameter("email");
-		//利用正则表达式（可改进）验证邮箱是否符合邮箱的格式
-	    if(!email.matches("^\\w+@(\\w+\\.)+\\w+$")){
-	        return state;
-	    }else {
-	    	String code = CodeUtil.generateUniqueCode();
-	    	state = userServiceImpl.addUser(username, password, email, code);
-	    	new Thread(new MailUtil(email, code)).run();
-	    }
+		// 利用正则表达式（可改进）验证邮箱是否符合邮箱的格式
+		if (!email.matches("^\\w+@(\\w+\\.)+\\w+$")) {
+			return state;
+		} else {
+			String code = CodeUtil.generateUniqueCode();
+			state = userServiceImpl.addUser(username, password, email, code);
+			new Thread(new MailUtil(email, code)).run();
+		}
 		return state;
 	}
+
 	@GetMapping("/regist/verified")
-	public String registVerified(HttpServletRequest request){
-		String code=request.getParameter("code");
+	public String registVerified(HttpServletRequest request) {
+		int state = 0;
+		String code = request.getParameter("code");
 		// 更新user表
-		int state = userServiceImpl.updateUser(code);
-		if(state > 0) {
-			return "Success.html";
-		}else {
-			return "Error.html";
+		state = userServiceImpl.updateUser(code);
+		if (state > 0) {
+			return "forward:/success";
+		} else {
+			System.out.println("in");
+			return "forward:/failure";
 		}
 	}
+
 	@RequestMapping("/login/verify")
 	@ResponseBody
-	public int loginVerify(HttpServletRequest request){
+	public int loginVerify(HttpServletRequest request) {
 		int statu = 0;
 		String username = request.getParameter("username");
 		String password = request.getParameter("password");
-		
+
 		Map map = userServiceImpl.loginUser(username, password);
-		if(map != null && map.get("username")!=null) {
-			if(map.get("state").toString().equals("0")) {
+		if (map != null && map.get("username") != null) {
+			if (map.get("state").toString().equals("0")) {
 				statu = 2;
-			}else {
+			} else {
 				statu = 1;
 			}
 		}
---
+++ src/main/resources/templates/js/ip.js
@@ -0,0 +1,6 @@
+//const ip = localhost
+//var ip = 106.13.130.77
+var ip = (function(){
+//	return 'localhost'
+	return '106.13.130.77'
+})()
